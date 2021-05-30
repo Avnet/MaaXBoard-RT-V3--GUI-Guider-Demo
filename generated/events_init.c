8e7bfab3414c2978414b5a9d2d7e8b5d83317b27
Juan Bernardo Tamez Pena <jbtamezpena@witekio.com>
2021-05-30 01:48:26 -0700

    revision 11

--- generated/events_init.c
+++ generated/events_init.c
@@ -77,8 +77,8 @@ static void screen0_MENU_imgbtn_5event_handler(lv_obj_t * obj, lv_event_t event)
 	{
 	case LV_EVENT_RELEASED:
 	{
-		setup_scr_screen3_USB2(&guider_ui);
-		lv_scr_load_anim(guider_ui.screen3_USB2, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
+		setup_scr_screen3_USB(&guider_ui);
+		lv_scr_load_anim(guider_ui.screen3_USB, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
 	}
 		break;
 	default:
@@ -229,8 +229,8 @@ static void screen2_WIFI_right_btnevent_handler(lv_obj_t * obj, lv_event_t event
 	{
 	case LV_EVENT_RELEASED:
 	{
-		setup_scr_screen3_USB2(&guider_ui);
-		lv_scr_load_anim(guider_ui.screen3_USB2, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
+		setup_scr_screen3_USB(&guider_ui);
+		lv_scr_load_anim(guider_ui.screen3_USB, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
 	}
 		break;
 	default:
@@ -260,7 +260,7 @@ void events_init_screen2_WIFI(lv_ui *ui)
 	lv_obj_set_event_cb(ui->screen2_WIFI_home_btn, screen2_WIFI_home_btnevent_handler);
 }
 
-static void screen3_USB2_left_btnevent_handler(lv_obj_t * obj, lv_event_t event)
+static void screen3_USB_left_btnevent_handler(lv_obj_t * obj, lv_event_t event)
 {
 	switch (event)
 	{
@@ -275,66 +275,29 @@ static void screen3_USB2_left_btnevent_handler(lv_obj_t * obj, lv_event_t event)
 	}
 }
 
-static void screen3_USB2_right_btnevent_handler(lv_obj_t * obj, lv_event_t event)
-{
-	switch (event)
-	{
-	case LV_EVENT_RELEASED:
-	{
-		setup_scr_screen4_AV(&guider_ui);
-		lv_scr_load_anim(guider_ui.screen4_AV, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
-	}
-		break;
-	default:
-		break;
-	}
-}
-
-static void screen3_USB2_home_btnevent_handler(lv_obj_t * obj, lv_event_t event)
-{
-	switch (event)
-	{
-	case LV_EVENT_RELEASED:
-	{
-		setup_scr_screen0_MENU(&guider_ui);
-		lv_scr_load_anim(guider_ui.screen0_MENU, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
-	}
-		break;
-	default:
-		break;
-	}
-}
-
-void events_init_screen3_USB2(lv_ui *ui)
-{
-	lv_obj_set_event_cb(ui->screen3_USB2_left_btn, screen3_USB2_left_btnevent_handler);
-	lv_obj_set_event_cb(ui->screen3_USB2_right_btn, screen3_USB2_right_btnevent_handler);
-	lv_obj_set_event_cb(ui->screen3_USB2_home_btn, screen3_USB2_home_btnevent_handler);
-}
-
-static void screen3_USB_left_btnevent_handler(lv_obj_t * obj, lv_event_t event)
-{
-	switch (event)
-	{
-	case LV_EVENT_PRESSED:
-	{
-		setup_scr_screen0_MENU(&guider_ui);
-		lv_scr_load_anim(guider_ui.screen0_MENU, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
-	}
-		break;
-	default:
-		break;
-	}
-}
-
 static void screen3_USB_right_btnevent_handler(lv_obj_t * obj, lv_event_t event)
 {
 	switch (event)
 	{
-	case LV_EVENT_PRESSED:
+	case LV_EVENT_RELEASED:
 	{
 		setup_scr_screen4_AV(&guider_ui);
 		lv_scr_load_anim(guider_ui.screen4_AV, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
+	}
+		break;
+	default:
+		break;
+	}
+}
+
+static void screen3_USB_home_btnevent_handler(lv_obj_t * obj, lv_event_t event)
+{
+	switch (event)
+	{
+	case LV_EVENT_RELEASED:
+	{
+		setup_scr_screen0_MENU(&guider_ui);
+		lv_scr_load_anim(guider_ui.screen0_MENU, LV_SCR_LOAD_ANIM_NONE, 0, 0, true);
 	}
 		break;
 	default:
@@ -346,6 +309,7 @@ void events_init_screen3_USB(lv_ui *ui)
 {
 	lv_obj_set_event_cb(ui->screen3_USB_left_btn, screen3_USB_left_btnevent_handler);
 	lv_obj_set_event_cb(ui->screen3_USB_right_btn, screen3_USB_right_btnevent_handler);
+	lv_obj_set_event_cb(ui->screen3_USB_home_btn, screen3_USB_home_btnevent_handler);
 }
 
 static void screen4_AV_left_btnevent_handler(lv_obj_t * obj, lv_event_t event)
