15286fc42404f8b62445146d60f9a08f57ab9af5
Juan Bernardo Tamez Pena <jbtamezpena@witekio.com>
2021-05-31 21:01:00 -0700

    revision 18

--- generated/setup_scr_screen3_USB.c
+++ generated/setup_scr_screen3_USB.c
@@ -540,7 +540,7 @@ void setup_scr_screen3_USB(lv_ui *ui){
 
 	//Write codes screen3_USB_font_label
 	ui->screen3_USB_font_label = lv_label_create(ui->screen3_USB, NULL);
-	lv_label_set_text(ui->screen3_USB_font_label, "test");
+	lv_label_set_text(ui->screen3_USB_font_label, "");
 	lv_label_set_long_mode(ui->screen3_USB_font_label, LV_LABEL_LONG_BREAK);
 	lv_label_set_align(ui->screen3_USB_font_label, LV_LABEL_ALIGN_CENTER);
 
@@ -555,7 +555,6 @@ void setup_scr_screen3_USB(lv_ui *ui){
 	lv_style_set_bg_grad_dir(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
 	lv_style_set_bg_opa(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, 255);
 	lv_style_set_text_color(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, lv_color_make(0x00, 0x00, 0x00));
-	lv_style_set_text_font(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, &lv_font_inconsolata_light_18);
 	lv_style_set_text_letter_space(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, 2);
 	lv_style_set_pad_left(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, 0);
 	lv_style_set_pad_right(&style_screen3_USB_font_label_main, LV_STATE_DEFAULT, 0);
